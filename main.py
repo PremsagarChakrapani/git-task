ca31ac1b3645149987eae7d9f016db33ff59fb9d
PremsagarChakrapani <104374523+PremsagarChakrapani@users.noreply.github.com>
2023-04-13 20:04:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ Bunch of functions
 
 def add(number_a:int, number_b:int):
     """
-    This function adds 2 numbers
+    This function adds 2 numbers.
+    the above was the task assigned. since the code was corrected when o forked it, the 
+    coordinators asked me to write a small change in code for the others to fix.
     """
     return number_a  * number_b 
     
